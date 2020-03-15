cbddef6c29f09170df2748af29321c7be269278b
matheusps <cout.matheusps@gmail.com>
2020-03-14 22:31:36 -0300

    Added docs

---
+++ docs/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let colors = [
+    'primary',
+    'purple',
+    'yellow',
+    'red',
+    'cyan',
+    'pink',
+    'orange',
+    'green',
+  ]
+</script>
+
+<main class="h-screen bg-base text-base-contrast px-5">
+  <ul>
+    {#each colors as color}
+      <li class={`text-${color} text-5xl`}>{color}</li>
+    {/each}
+  </ul>
+</main>

b2527442eaf5105accbbaf953d8b42414fe30f58
matheusps <cout.matheusps@gmail.com>
2020-03-14 22:01:41 -0300

    💣 Fist commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Source Sans Pro', 'sans-serif'],
+      body: ['Source Sans Pro', 'sans-serif'],
+    },
+    colors: {
+      base: {
+        default: 'var(--color-base)',
+        contrast: 'var(--color-base--contrast)',
+      },
+      primary: {
+        default: 'var(--color-primary)',
+        contrast: 'var(--color-primary--contrast)',
+        faded: 'var(--color-primary--focus)',
+        hover: 'var(--color-primary--hover)',
+        focus: 'var(--color-primary--focus)',
+      },
+      cyan: 'var(--color-cyan)',
+      green: 'var(--color-green)',
+      orange: 'var(--color-orange)',
+      pink: 'var(--color-pink)',
+      purple: 'var(--color-purple)',
+      red: 'var(--color-red)',
+      yellow: 'var(--color-yellow)',
+      white: '#fff',
+      black: '#000',
+    },
+  },
+  variants: {},
+  plugins: [],
+}

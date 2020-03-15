a4a686c8cb4ddf1a74dccbdac0748b1a6aefa694
matheusps <cout.matheusps@gmail.com>
2020-03-15 10:38:56 -0300

    Created semantic colors

--- docs/src/App.svelte
+++ docs/src/App.svelte
@@ -1,21 +1,22 @@
 <script>
   import Selector from './Selector.svelte'
   export let colors = [
+    'base',
+    'box',
     'primary',
-    'purple',
-    'yellow',
-    'red',
-    'cyan',
-    'pink',
-    'orange',
-    'green',
+    'secondary',
+    'info',
+    'success',
+    'warning',
+    'error',
+    'highlight',
   ]
 </script>
 
 <main class="h-screen bg-base text-base-contrast px-5">
   <section class="mb-24">
-    <h1 class="text-6xl text-pink text-center">🦇 Dracula With Tail 🌊</h1>
-    <h2 class="text-4xl text-yellow text-center">Awesome tailwind theme</h2>
+    <h1 class="text-6xl text-primary text-center">🦇 Dracula With Tail 🌊</h1>
+    <h2 class="text-4xl text-secondary text-center">Awesome tailwind theme</h2>
   </section>
 
   <!-- Selectors -->
--- docs/src/Selector.svelte
+++ docs/src/Selector.svelte
@@ -6,6 +6,6 @@
 
 <button
   on:click
-  class={`bg-${variant} mr-4 w-32 h-32 text-xl text-primary-faded rounded-lg ${isSelected ? 'border-4 border-white' : ''}`}>
+  class={`bg-${variant} text-${variant}-contrast mr-4 w-32 h-32 text-xl rounded-lg`}>
   <slot />
 </button>
